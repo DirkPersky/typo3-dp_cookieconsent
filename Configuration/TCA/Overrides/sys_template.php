d9c62559305e6bd53f2b158aeb6faf794ae9d3e1
Dirk <dirk.persky@gmail.com>
2019-11-04 18:36:54 +0100

    composer bug test1

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,6 +1,5 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-
 // Add Plugin Configs
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('dp_cookieconsent', 'Configuration/TypoScript', 'CookieConsent');
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,15 +2,10 @@
 defined('TYPO3_MODE') or die();
 
 (function () {
-
-// Register Plugin and name SPaces
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'DirkPersky.' . 'dp_cookieconsent',
-    'CookieConsent',
-    'CookieConsent'
-);
-
-
-
-
+    // Register Plugin and name SPaces
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        'DirkPersky.' . 'dp_cookieconsent',
+        'CookieConsent',
+        'CookieConsent'
+    );
 })();
