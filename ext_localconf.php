032880d228e8ca4284fb35337ce4878ad690adf4
whiteroom <mail@whiteroom.cc>
2019-11-04 17:32:25 +0100

    Fix compat. issues

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,2 +1,16 @@
 <?php
 defined('TYPO3_MODE') or die();
+
+(function () {
+
+// Register Plugin and name SPaces
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'DirkPersky.' . 'dp_cookieconsent',
+    'CookieConsent',
+    'CookieConsent'
+);
+
+
+
+
+})();
--- ext_tables.php
+++ ext_tables.php
@@ -1,10 +1,2 @@
 <?php
 defined('TYPO3_MODE') or die();
-// Register Plugin and name SPaces
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'DirkPersky.' . $_EXTKEY,
-    'CookieConsent',
-    'CookieConsent'
-);
-// Add Plugin Configs
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'CookieConsent');
