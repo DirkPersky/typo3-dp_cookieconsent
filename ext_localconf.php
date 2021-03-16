250d125577c7dac83d2bb9998c724c8a2f9d49ae
Dirk Persky <dirk.persky@gmail.com>
2021-03-16 07:39:41 +0100

    configuration files best practices

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,14 +9,11 @@
  * @license    MIT
  */
 
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
-
 defined('TYPO3_MODE') or die();
-
 $boot = function () {
     // Register FE namespace
-    ExtensionUtility::configurePlugin(
-        'DirkPersky.' . 'dp_cookieconsent',
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'DpCookieconsent',
         'CookieConsent',
         []
     );
