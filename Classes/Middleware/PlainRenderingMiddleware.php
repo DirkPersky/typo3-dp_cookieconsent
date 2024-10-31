43187b51f24ac0810728698a6ac28048e8e9360e
Dirk Persky <dirk.persky@gmail.com>
2024-10-31 12:47:15 +0100

    #156 Code checkup for TYPO3 v12 compatibility

--- Classes/Middleware/PlainRenderingMiddleware.php
+++ Classes/Middleware/PlainRenderingMiddleware.php
@@ -19,6 +19,7 @@ use TYPO3\CMS\Core\TypoScript\AST\Node\ChildNode;
 use TYPO3\CMS\Core\TypoScript\AST\Node\RootNode;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Information\Typo3Version;
 
 class PlainRenderingMiddleware implements MiddlewareInterface
 {
@@ -32,31 +33,56 @@ class PlainRenderingMiddleware implements MiddlewareInterface
             return $handler->handle($request);
         }
 
+        // get frontend typoscript
         $frontendTyposcript = $request->getAttribute('frontend.typoscript');
-
-        $configArray = $frontendTyposcript->getConfigArray();
-        $configArray['debug'] = 0;
-        $configArray['disableAllHeaderCode'] = 1;
-        $configArray['disableCharsetHeader'] = 0;
-        $frontendTyposcript->setConfigArray($configArray);
-
         // prepare rendering overwrite
         // set UID
         $uid = $request->getQueryParams()[self::namespace]['content'];
-
-        $pageArray = $frontendTyposcript->getPageArray();
-        $pageArray['10'] = 'COA';
-        $pageArray['10.'] = [
-            '10' => 'RECORDS',
+        // get current typo3 version
+        $version = GeneralUtility::makeInstance(Typo3Version::class)->getMajorVersion();
+        // prepare page rendering
+        $pageArray = [
+            '10' => 'COA',
             '10.' => [
-                'tables' => 'tt_content',
-                'source' => "tt_content_{$uid}"
+                '10' => 'RECORDS',
+                '10.' => [
+                    'tables' => 'tt_content',
+                    'source' => "tt_content_{$uid}"
+                ]
             ]
         ];
-        $frontendTyposcript->setPageArray($pageArray);
+        // get frontend typoscript
+        $setup = $frontendTyposcript->getSetupArray();
+        // change fluid Layout to remove Wrap
+        if (isset($setup['lib.']['contentElement.'])) $setup['lib.']['contentElement.']['layoutRootPaths.'][999] = 'EXT:dp_cookieconsent/Resources/Private/Overwrite/Layouts/';
+        // TODO Check: Working T3 v 13?
+        if ($version >= 13) {
+            $configArray = $frontendTyposcript->getConfigArray();
+            $configArray = array_merge($configArray, [
+                'debug' => 0,
+                'disableAllHeaderCode' => 1,
+                'disableCharsetHeader' => 0
+            ]);
+            // disable svgstore
+            if (isset($configArray['svgstore.'])) $configArray['svgstore.']['enabled'] = 0;
+            $frontendTyposcript->setSetupArray($setup);
+            $frontendTyposcript->setConfigArray($configArray);
+            $frontendTyposcript->setPageArray($pageArray);
+        } else {
+            // fallback to older TYPO3 versions
+            $setup['config.'] = array_merge($setup['config.'], [
+                'debug' => 0,
+                'disableAllHeaderCode' => 1,
+                'disableCharsetHeader' => 0
+            ]);
+            // disable svgstore
+            if (isset($setup['config.']['svgstore.'])) $setup['config.']['svgstore.']['enabled'] = 0;
+            // set TypoScript updates
+            $frontendTyposcript->setSetupArray($setup);
+            $controller->pSetup = $pageArray;
+        }
 
         $request = $request->withAttribute('frontend.typoscript', $frontendTyposcript);
-
         return $handler->handle($request);
     }
 }
